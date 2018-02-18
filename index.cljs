c9bb2e1ac6ab550a48ea09e5b1d449c1e9a20cd0
MichelML <michmoreau.l@gmail.com>
2018-02-18 14:16:40 -0500

    use when-not

--- index.cljs
+++ index.cljs
@@ -10,9 +10,8 @@
 (def release-type (-> (.. process -argv) last string/lower-case string/trim))
 (def cmds [(str "npm version " release-type) "git push origin master --tags" "npm publish"])
 
-(if-not (some #(= % release-type) ["patch" "minor" "major"])
-  (do 
+(when-not (some #(= % release-type) ["patch" "minor" "major"])
     (throw "Wrong type of release provided. You must specify a patch, minor, or major release.")
-    (.exit process 1)))
+    (.exit process 1))
 
 (doseq [cmd cmds] (js/console.log (.write decoder (exec-sync cmd))))
