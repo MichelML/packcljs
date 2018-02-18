3d580ddedab5a9c7f245d0a24723e68619f9c639
MichelML <michmoreau.l@gmail.com>
2018-02-18 12:05:06 -0500

    try decoder write

--- index.cljs
+++ index.cljs
@@ -3,9 +3,8 @@
 (require '[clojure.string :as string])
 
 (def process (node/require "process"))
-(def string-decoder (.. (node/require "string_decoder") -StringDecoder))
+(def decoder ((.. (node/require "string_decoder") -StringDecoder). "utf8")
 (def exec-sync (.. (node/require "child_process") -execSync))
-(def decoder (string-decoder. "utf8"))
 (def release-type (-> (.. process -argv) last string/lower-case string/trim))
 (def cmds [(str "npm version " release-type) "git push origin master --tags" "npm publish"])
 
