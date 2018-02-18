ab184a7ac111f4536a0bb2439b6df527da6d4c67
MichelML <michmoreau.l@gmail.com>
2018-02-18 11:39:53 -0500

    fix conditional

--- index.cljs
+++ index.cljs
@@ -1,15 +1,15 @@
 #! /usr/bin/env lumo
 (require '[cljs.nodejs :as node])
-(refer 'clojure.string :only '[lower-case])
+(require '[clojure.string :as string])
 
 (def process (node/require "process"))
 (def exec-sync (.. (node/require "child_process") -execSync))
-(def release-type (-> argv last lower-case))
-(def cmds [(str "npm version" release-type) "git push origin master --tags" "npm publish"])
+(def release-type (-> (.. process -argv) last string/lower-case string/trim))
+(def cmds [(str "npm version " release-type) "git push origin master --tags" "npm publish"])
 
-(if-not (get ["patch" "minor" "major"] release-type)
+(if-not (some #(= % release-type) ["patch" "minor" "major"])
   (do 
-    (js/throw "Wrong type of release provided. You must specify a patch, minor, or major release.")
+    (throw "Wrong type of release provided. You must specify a patch, minor, or major release.")
     (.exit process 1)))
 
 (doseq [cmd cmds] (js/console.log (exec-sync cmd #{:encoding "utf-8"})))
